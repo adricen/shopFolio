4bba319a97d6b992a2940a5a5e2b29aa3405a139
Adricen <acentonze@wanadev.fr>
2024-10-08 21:54:58 +0200

    initial project

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation())
+</script>
+
+<template>
+    <header>
+        <ul>
+            <li v-for="item in navigation" :key="item.id">
+                <nuxt-link :to="item._path">{{ `${item.icon ?? ''} ${item.title}` }}</nuxt-link>
+            </li>
+        </ul>
+        <pre>{{ navigation }}</pre>
+    </header>
+</template>
